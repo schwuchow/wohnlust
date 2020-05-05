dcc6c98fd0e436894f55026291b909ecda877f59
Judali <jschwuchow@googlemail.com>
2020-05-05 12:12:08 +0200

    fixed uncontrolled -> controlled searchbar input

--- src/features/searchbar/reducerSearchbar.js
+++ src/features/searchbar/reducerSearchbar.js
@@ -1,6 +1,6 @@
 import { CHANGE_SEARCH_FIELD, CHANGE_CITY_ON_DISPLAY } from '../../actionConstants';
 
-export const searchbarReducer = (state = {}, action) => {
+export const searchbarReducer = (state = {searchField: '', cityOnDisplay: ''}, action) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD: return Object.assign({}, state, { searchField: action.payload });
         case CHANGE_CITY_ON_DISPLAY: return Object.assign({}, state, { cityOnDisplay: action.payload })
