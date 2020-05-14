d1e5a18aa998c0b281901f76167eee329324ce30
Judali <jschwuchow@googlemail.com>
2020-05-14 18:13:28 +0200

    serve to client/build

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.DB_CONNECTION, () => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
