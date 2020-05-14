bd8900e71cec6b8dbcaeda87785203de41b2c685
Judali <jschwuchow@googlemail.com>
2020-05-14 10:23:16 +0200

    added helmet, compression, debug modules, updated packages

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,10 @@
+const debug = require('debug');
+const log = debug('http');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,18 +12,19 @@ require('dotenv').config();
 const appartmentsRoute = require('./routes/appartments');
 
 // Middlewares
+app.use(compression);
+app.use(helmet())
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/appartments', appartmentsRoute);
-// app.use('/api/user', authRoute);
 
 // Connect to DB
 mongoose.set('debug', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.DB_CONNECTION, () => {
-    console.log('connected to DB');
+    log('connected to DB');
 });
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
